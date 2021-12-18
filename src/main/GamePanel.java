447070e60a81093a952bebc0af44fb387f919185
Eduardo <garayarnaldos@gmail.com>
2021-12-18 17:47:10 +0100

    Working on Game Loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -46,6 +46,9 @@ public class GamePanel extends JPanel implements Runnable {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(100,100,tileSize,tileSize);
+        g2.dispose();
     }
 
 }
