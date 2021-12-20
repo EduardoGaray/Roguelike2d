fc8054109b91a08ac87815381cb73559fa1b1ee7
EduardoGaray <garayarnaldos@gmail.com>
2021-12-20 15:49:42 +0100

    working on procedural generation of tilemaps

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -5,5 +5,8 @@ import java.awt.image.BufferedImage;
 public class Tile {
     public BufferedImage image;
     public boolean collision = false;
+    public String name;
+    public String type;
+    public int x,y;
 
 }
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -9,82 +9,82 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
 
 public class TileManager {
     GamePanel gp;
+    //Right now this is the array that we use to create the current game map, later on we must create every map in a separate array and save/load them as needed
     Tile[] tile;
-    int mapTileNum[][];
 
     public TileManager(GamePanel gp) {
+        //Here we initialize the process to create the map
         this.gp = gp;
-        tile = new Tile[10];
-        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+        tile = new Tile[gp.maxScreenCol * gp.maxScreenRow];
+        int area = gp.maxScreenCol * gp.maxScreenRow;
+        System.out.println(area);
         getTileImage();
         loadMap();
     }
 
+    //this method generates basic tiles randomly, we can work on a more complex system for map generation from here
     public void getTileImage() {
         try {
-            tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass00.png"));
-            tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
-            tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
-
+            int col = 0;
+            int row = 0;
+            int x = 1;
+            int y = 1;
+            for (int i = 0; i<tile.length; i++ ){
+                tile[i] = new Tile();
+                tile[i].x = x;
+                tile[i].y = y;
+                final String[] name = {"grass00","earth","water00","tree"};
+                Random random = new Random();
+                int index = random.nextInt(name.length);
+                tile[i].name = name[index];
+                tile[i].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/"+tile[i].name+".png")));
+                col++;
+                x += 1;
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    x = 1;
+                    row++;
+                    y += 1;
+                }
+                System.out.println(tile[i].name+" "+tile[i].x+" "+tile[i].y);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void loadMap() {
-        try {
-            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            int col = 0;
-            int row = 0;
-
-            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
-
-                String line = br.readLine();
-
-                while (col < gp.maxScreenCol) {
-
-                    String numbers[] = line.split(" ");
-                    int num = Integer.parseInt(numbers[col]);
-                    mapTileNum[col][row] = row;
-                    col++;
-                }
-                if (col == gp.maxScreenCol) {
-                    col = 0;
-                    row++;
-                }
-            }
-            br.close();
-
-        } catch (Exception e) {
+        for (int i = 0; i < tile.length; i++) {
 
         }
-    }
 
+    }
+    //This method renders the tile array.image property
     public void draw(Graphics2D g2) {
         int col = 0;
         int row = 0;
         int x = 0;
         int y = 0;
+        int cont = 0;
         while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
-
-            int tileNum = mapTileNum[col][row];
-
-            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            g2.drawImage(tile[cont].image, x, y, gp.tileSize, gp.tileSize, null);
+            cont++;
             col++;
             x += gp.tileSize;
-
             if (col == gp.maxScreenCol) {
                 col = 0;
                 x = 0;
                 row++;
                 y += gp.tileSize;
+            }
+            if (cont >= tile.length) {
+                cont = 0;
             }
         }
     }
