d263b35a306ec64478693ef94a09a74c7a96a287
EduardoGaray <garayarnaldos@gmail.com>
2021-12-20 10:54:22 +0100

    working on tiles system

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
+import tile.TileManager;
+
 import javax.swing.*;
 
 // video 2 min 14:24
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -6,44 +6,49 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
 
-
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
         getTileImage();
-
     }
-    public void getTileImage() {
-        try{
-            tile[0] = new Tile();
-            tile[0].image =  ImageIO.read(getClass().getResourceAsStream("/tiles/grass00.png"));
-            tile[1] = new Tile();
-            tile[1].image =  ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
-            tile[2] = new Tile();
-            tile[2].image =  ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
 
-        } catch (IOException e){
+    public void getTileImage() {
+        try {
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass00.png"));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void draw(Graphics2D g2){
-        int xpos = 0;
-        int ypos = 0;
-        int id = 1;
-        int counter = 0;
-        for (int i = 0; i <gp.maxScreenCol * gp.maxScreenRow; i++, xpos += gp.tileSize, counter++){
-            if(counter == gp.maxScreenCol){
-                id = 0;
-                ypos += gp.tileSize;
-                xpos = 0;
-                counter = 0;
+
+    public void draw(Graphics2D g2) {
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+        int id = 0;
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            col ++;
+            x += gp.tileSize;
+            if(col == gp.maxScreenCol){
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
             }
-            g2.drawImage(tile[id].image,xpos,ypos,gp.tileSize,gp.tileSize,null);
+            id ++;
         }
     }
 }
